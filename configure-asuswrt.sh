8689b438ba6d923f607f3723e44457e861c2d2aa
blackfuel <blackfuel@github.com>
2017-07-15 08:14:47 -0400

    make separate script to configure AsusWRT directories

---
+++ configure-asuswrt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# need root privs to create the directories for AsusWRT
+
+[ ! -d /opt ] && sudo mkdir -p /opt
+[ ! -h /opt/brcm ] && sudo ln -sf $HOME/asuswrt-merlin/tools/brcm /opt/brcm
+[ ! -h /opt/brcm-arm ] && sudo ln -sf $HOME/asuswrt-merlin/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
+[ ! -d /projects/hnd/tools/linux ] && sudo mkdir -p /projects/hnd/tools/linux
+[ ! -h /projects/hnd/tools/linux/hndtools-arm-linux-2.6.36-uclibc-4.5.3 ] && sudo ln -sf /opt/brcm-arm /projects/hnd/tools/linux/hndtools-arm-linux-2.6.36-uclibc-4.5.3
+
+#sudo apt-get install  xutils-dev libltdl-dev automake1.11
+
